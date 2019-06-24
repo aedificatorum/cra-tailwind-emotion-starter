26261e904c1c8e96a485999d4ab018d8d0f9b858
Tim Addison <git@tjaddison.com>
2019-06-24 19:26:18 -0400

    Add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/tailwind.out.css";
 
 function App() {
   return (
